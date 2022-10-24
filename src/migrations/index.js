fa1de46d8cd0522b7135a06b387b00bb77f30193
mosukung <satawat.rattha@gmail.com>
2022-10-25 01:15:05 +0700

    update migration

--- src/migrations/index.js
+++ src/migrations/index.js
@@ -4,12 +4,22 @@ const employees = require('../schemas/employees')
 const requests = require('../schemas/requests')
 const users = require('../schemas/users')
 
+const userRepo = require('../repositories/user-pgdb')
+const roles = require('../models/roles')
+const passwords = require('../libs/passwords')
+
 async function main() {
     await requests.sync({ force: true })
     await users.sync({ force: true })
     await companies.sync({ force: true })
     await admins.sync({ force: true })
     await employees.sync({ force: true })
+
+    await userRepo.create({
+        username: 'admin',
+        password: passwords.hash('admin'),
+        role: roles.salaryHero
+    })
 }
 
 main()
